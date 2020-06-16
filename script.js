0cd3df48f4e3ec44ec33ac5047d774ce29a6e496
Nick Mardon <nickmardondigital@gmail.com>
2020-06-15 19:06:12 -0700

    defined question array and other variables, tweaked html and css

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //Pseudo Code
-//TODO: create array of questions 
-//TODO: assign variables to the question text and answer buttons
-//TODO: assign variable to correct/wrong h3 tag 
-//TODO: create nested array of answers for each question
-//TODO: give answers boolean values
+//TODO: create array of questions +1
+//TODO: assign variables to the question text and answer buttons +1
+//TODO: assign variable to correct/wrong h3 tag +1
+//TODO: create nested array of answers for each question +1
 //TODO: use for loop and math radom math floor methods to pick random questions
 //TODO: use nested for loop to randomly display coresponding answers in buttons
-//TODO: create score variable
+//TODO: create score variable +1
 //TODO: create timer
 //TODO: when timer hits 0 game is over 
 //TODO: if user clicks correct answer add to score
@@ -14,34 +13,50 @@
 //TODO: when the game is over create input for user to enter score
 //TODO: use local storage to save user scores
 
+
+//Array of questions and answers 
+//right answers are in index 1
 var questionArr = [
     {question: "How do you write an IF statement in JavaScript?",
-    answers: ["if = 5 then", "if i == 5 then", "if(i==5)", "if i = 5"]
+    answers: ["if = 5 then", "if(i==5)","if i == 5 then", "if i = 5"]
     },
     {question: "How does a WHILE loop start?",
-    answers: ["while i < 5", "while [i<5]", "while(i)<5", "(while i < 5)"]
+    answers: ["while i < 5", "while (i<5)", "while(i)<5", "(while i < 5)"]
     },
     {question: "What HTML tag links JavaScript to HTML?",
-    answers: ["<JavaScript>", "<script.js>", "<script>", "<Java>"]
+    answers: ["<JavaScript>", "<script>", "<script.js>", "<Java>"]
     },
     {question: "Which of the following is NOT part of a for loop",
-    answers: ["variable", "condition", "increment", "boolean"]
+    answers: ["variable", "boolean", "condition", "increment"]
     },
     {question: "How do you test for two conditions",
-    answers: ["while i < 5 || i > 9", "while [i<5 && i >9]", "while(5 < i < 9)", "while (i < 5 && i > 9)"]
+    answers: ["while i < 5 || i > 9", "while (i < 5 && i > 9)", "while [i<5 && i >9]", "while(5 < i < 9)"]
     },
     {question: "Which of the following is NOT a JavaScript framework?",
-    answers: ["React.js", "Vue.js", "Go.js", "Node.js"]
+    answers: ["React.js", "Go.js", "Vue.js", "Node.js"]
     },
     {question: "How do you add a comment in JavaScript?",
-    answers: ["/*This is my comment*/", "<!--This is my comment-->", "!this is my comment!", "//This is my comment"]
+    answers: ["/*This is my comment*/", "//This is my comment", "<!--This is my comment-->", "!this is my comment!"]
     },
     {question: "How do you write an Array in JavaScript?",
-    answers: ["let arr = ()", "let arr = ()", "let arr = []", "let arr([])"]
+    answers: ["let arr = ()", "let arr = []", "let arr = ()", "let arr([])"]
     },
-    {question: "",
-    answers: ["let arr = ()", "let arr = ()", "let arr = []", "let arr([])"]
+    {question: "JavaScript and Java...",
+    answers: ["Are the Same", "Are different programming languages", "Are built off of eachother", "were created by the same person"]
+    },
+    {question: "Which of the following type of variable is visible everywhere in your JavaScript code?",
+    answers: ["Local Variable", "Global Variable", "Both", "Neither"]
+    },
+    {question: "Which of the following is a JavaScript method?",
+    answers: ["let", ".toUpperCase", "break", ".Math"]
+    },
+    {question: "Which of the following functions turns a string into an array?",
+    answers: [".slice()", ".split()", ".parse()", ".stringify()"]
     },
 ]
-
-console.log(questionArr[4].answers[2]);
+//Defining DOM variables
+var questionText = document.querySelector("#question");
+var answerBtns = document.querySelectorAll("answerBtn");
+var timer = document.querySelector("#timerSpan");
+var confirmAnswer = document.querySelector(".confirmAnswer");
+var score = 0;
